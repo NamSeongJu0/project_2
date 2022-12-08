75e6966a9cc609a3a849f5fc837116b80c3dab2b
NamSeongJu0 <skaehdwn0@naver.com>
2022-12-08 18:21:33 +0900

    12/08
    hum...

---
+++ jsfile/index.js
@@ -0,0 +1,28 @@
+// 윈도우 너비가 768px 밑에일 시 메뉴바 보임
+
+$(function(){
+    $(".a").click(function(){
+     $(".b").slideToggle();
+    });
+   });
+
+   $(window).resize(function(){
+    if(window.innerWidth>=768){
+       $('.b').show();
+    }
+    else{
+        $('.b').hide();
+    }
+   });
+
+   // li(BLOG)를 hover하면 안에 있던 subblog가 보여지는 기능
+   $(document).ready(function () {
+    $('.subblog').hide();
+
+    $('.blog').hover(function () {
+        $(this).children('.subblog').stop().slideDown();
+    }, function () {
+        $(this).children('.subblog').stop().slideUp();
+    });
+});
+   
