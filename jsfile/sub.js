756b147e5d80a300beb5cb8d446ac8d49c1b7f55
NamSeongJu0 <skaehdwn0@naver.com>
2023-01-03 17:59:30 +0900

    01/03
    ..mdmdkkdkdl

---
+++ jsfile/sub.js
@@ -0,0 +1,62 @@
+// 윈도우 너비가 768px 밑에일 시 메뉴바 보임
+
+$(function(){
+    $("#a").click(function(){
+     $(".b").slideToggle();
+    });
+   });
+
+   $(window).resize(function(){
+    if(window.innerWidth > 767){
+       $('.b').show();
+    }
+    else{
+        $('.b').hide();
+    }
+   });
+
+
+   // li(BLOG)를 hover하면 안에 있던 subblog가 보여지는 기능
+   $(document).ready(function () {
+    $('.subblog').hide();
+
+    $('.blog').hover(function () {
+        $(this).children('.subblog').stop().slideDown();
+    }, function () {
+        $(this).children('.subblog').stop().slideUp();
+    });
+});
+
+
+// 헤더 위치에 따른 변화
+$(document).ready(function (){
+    $(window).scroll(function () {
+        if ($(window).scrollTop() < 100 || $(window).scrollTop() > 700) {
+            $('header').fadeIn(500).css('backgroundColor', 'none');
+            $('#wrap').css('backgroundColor', 'rgba(0, 0, 0, 0.9)');
+        } else {
+            $('header').fadeOut(500).css('backgroundColor', 'rgba(0, 0, 0, 0.9)');
+
+        }
+    });
+})
+
+
+// 스크롤 위치에 따른 탑버튼 변화
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 700) {
+            $('.topbtn').fadeIn(500);
+        } else {
+            $('.topbtn').fadeOut(500);
+        }
+    });
+
+    
+// 탑 버튼 속도 조절
+    $('.topbtn').click(function (event) {
+        event.preventDefault();
+        $('html, body').animate({ scrollTop: 0 }, 1800);
+    });
+});
+
