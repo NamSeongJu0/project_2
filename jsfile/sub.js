7617731ad37bdb1c0981c3e1e5b2d2b8ffeec173
NamSeongJu0 <skaehdwn0@naver.com>
2023-01-06 19:40:48 +0900

    01/06
    이제 슬슬 반응형 들어가자!

--- jsfile/sub.js
+++ jsfile/sub.js
@@ -1,10 +1,21 @@
 // 슬라이드 토글
 $(function () {
     $('.btn button').click(function () {
-        $('.btn button').addClass('on');
+        $(this).toggleClass('on')
         $('.form1').slideToggle(800, 'swing');
     });
 });
+
+$(function(){
+    $('.circle1').hover(function(){
+        $('.circle1 a i').css('color', 'red');
+        $('.circle1').css('background-color', 'white');
+    }, function(){
+        $('.circle1 i').css('color', 'white');
+        $('.circle1').css('background-color', 'rgb(255, 118, 94)');
+    });
+   
+})
 
 
 // 버튼 클릭 유도
